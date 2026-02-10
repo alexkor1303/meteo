0e6e933d9896fee743cd440689b697c0aa0e9122
unknown <alexkor1303@yandex.ru>
2026-02-10 17:27:42 +0300

    add:start project structure

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-
+import { initApp } from "./init.js";
+initApp();
---
+++ src/api/ApiKeyAndHost.js
@@ -0,0 +1,2 @@
+export const apikey = "f086d8d5a099500a91ad5adbabcc1323";
+export const baseUrl = "https://api.openweathermap.org";
